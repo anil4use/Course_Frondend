dd92bf394dafd69dd343056104b96f4e4c435342
anil4use <anil4use@gmail.com>
2023-06-28 11:34:54 +0530

    login sign UI change

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,56 +1,102 @@
-import { Box, Button, Container, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, Input, Stack, Text, } from '@chakra-ui/react'
+import {
+    Box,
+    Button,
+    Flex,
+    FormControl,
+    useColorModeValue,
+    FormErrorMessage,
+    FormLabel,
+    Heading,
+    Input,
+    Stack,
+    Text,
+    InputGroup,
+    InputRightElement,
+} from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { login } from '../../redux/actions/UserAction'
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 const Login = () => {
     const [Email, SetEamil] = useState("")
     const [Password, SetPassword] = useState("")
-    const isError = Input === ''
+    const [showPassword, setShowPassword] = useState(false);
 
-    const forgetHandler = (e) => {
-        console.log("forget password")
-    }
+    const isError = Input === ''
     const dispatch = useDispatch()
     const SumbitHandler = (e) => {
         e.preventDefault();
         dispatch(login(Email, Password));
     }
+
     return (
-        <Container mt={'50'} h={'95vh'} paddingY={'16'}>
-            <Heading textAlign={'center'}>Free Course RR LOGIN</Heading>
+        <>
 
-            <Box p={4} >
-                <form onSubmit={SumbitHandler} >
-                    <FormControl isInvalid={isError}>
-                        <Stack spacing={'4'} justifyContent={'center '}  >
-
-                            <FormLabel>Email</FormLabel>
-                            <Input required placeholder='exampl@gamil.com' type='email' value={Email} onChange={e => SetEamil(e.target.value)} />
-                            {!isError ? (
-                                <FormHelperText>
-                                    {/* Enter the email you'd like to receive the newsletter on. */}
-                                </FormHelperText>
-                            ) : (
-                                <FormErrorMessage>Email is required.</FormErrorMessage>
-                            )}
-                            <FormLabel>Password</FormLabel>
-                            <Input required placeholder='password' id='s' type='password' value={Password} onChange={e => SetPassword(e.target.value)} />
-                            {/* <Link  to={"/forgetpassward"}>
-                                    <Button disabled mt={'3'} variant={'link'} colorScheme='linkedin' onClick={forgetHandler}>forgot passward?</Button>
-                                </Link> */}
-                            <br></br>
-
-                            <Button mt={'3'} color={'linkedin.300'} variant={'solid'} type='submit'>login Now</Button>
-
-                            <Text m={'4'}> You don't have a accout <Link to={"/register"}>   <Button color={'linkedin.300'} variant={'link'} onClick={forgetHandler}>click</Button></Link>here</Text>
+            <Flex
+                minH={'100vh'}
+                align={'center'}
+                justify={'center'}
+                bg={useColorModeValue('white', 'gray.800')}>
+                <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                    <Stack align={'center'}>
+                        <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+                        <Text fontSize={'lg'} color={'gray.600'}>
+                            to enjoy all of our cool features ✌️
+                        </Text>
+                    </Stack>
+                    <Box
+                        rounded={'lg'}
+                        bg={useColorModeValue('white', 'gray.700')}
+                        boxShadow={'lg'}
+                        p={8}>
+                        <Stack w={'sm'} spacing={4}>
+                            <form onSubmit={SumbitHandler}>
+                                <FormControl isInvalid={isError} id="email" isRequired>
+                                    <FormLabel>Email address</FormLabel>
+                                    <Input type='email' value={Email} onChange={e => SetEamil(e.target.value)} />
+                                    {!isError ? (
+                                        <></>
+                                    ) : (
+                                        <FormErrorMessage>Email is required.</FormErrorMessage>
+                                    )}
+                                </FormControl>
+                                <FormControl id="password" isRequired>
+                                    <FormLabel>Password</FormLabel>
+                                    <InputGroup>
+                                        <Input type={showPassword ? 'text' : 'password'} value={Password} onChange={e => SetPassword(e.target.value)} />
+                                        <InputRightElement h={'full'}>
+                                            <Button
+                                                variant={'ghost'}
+                                                onClick={() =>
+                                                    setShowPassword((showPassword) => !showPassword)
+                                                }>
+                                                {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                                            </Button>
+                                        </InputRightElement>
+                                    </InputGroup>
+                                </FormControl>
+                                <Stack mt={'3'} spacing={10} pt={2}>
+                                    <Button
+                                        loadingText="Submitting"
+                                        size="lg"
+                                        color={'linkedin.300'} variant={'solid'}
+                                        _hover={{
+                                            bg: 'gray.200',
+                                        }}
+                                        type={'submit'}>
+                                        Sign up
+                                    </Button>
+                                </Stack>
+                                <Stack pt={6}>
+                                    <Text > You don't have a accout <Link to={"/register"}>   <Button color={'linkedin.300'} variant={'link'} >click</Button></Link>here</Text>
+                                </Stack>
+                            </form>
                         </Stack>
-                    </FormControl>
-                </form>
-            </Box>
-
-
-        </Container>
+                    </Box>
+                </Stack>
+            </Flex>
+        </>
     )
 }
 
--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -1,8 +1,10 @@
-import { Avatar, Box, Button, Container, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, Input, Stack, Text, VStack } from '@chakra-ui/react'
+import { Avatar, Box, Button, Flex, useColorModeValue, FormControl, FormErrorMessage, FormLabel, Heading, Input, InputGroup, InputRightElement, Stack, Text, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { RegisterUser } from '../../redux/actions/UserAction'
 import { useDispatch } from 'react-redux'
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+
 const Register = () => {
     const [Email, SetEamil] = useState("")
     const [Name, SetName] = useState("")
@@ -11,9 +13,7 @@ const Register = () => {
     const [AvatarPP, SetAvatarPP] = useState("")
 
     const isError = Input === ''
-    const LoginHandler = () => {
-        console.log("login")
-    }
+ 
     const dispatch = useDispatch();
     const registerHandler = (e) => {
         e.preventDefault();
@@ -34,39 +34,92 @@ const Register = () => {
             SetAvatarPP(reader.result)
         }
     }
-    return (
-        <Container h={'95vh'}  paddingY={'14'}>
-            <Heading textAlign={'center'}>Free Course RR</Heading>
-                <Box >
-                    <form onSubmit={registerHandler}>
-                        <FormControl w={''} isInvalid={isError}>
-                        <Stack spacing={'2'} justifyContent={'center '}  >
 
-                            <VStack>
-                                <Avatar size={'lg'} src={AvatarPP} />
-                            </VStack>
-                            <FormLabel>Name</FormLabel>
-                            <Input required placeholder='Jonh Dev' type='text' value={Name} onChange={e => SetName(e.target.value)} />
-                            <FormLabel>Email</FormLabel>
-                            <Input required placeholder='exampl@gamil.com' type='email' value={Email} onChange={e => SetEamil(e.target.value)} />
-                            {!isError ? (
-                                <FormHelperText>
-                                    {/* Enter the email you'd like to receive the newsletter on. */}
-                                </FormHelperText>
-                            ) : (
-                                <FormErrorMessage>Email is required.</FormErrorMessage>
-                            )}
-                            <FormLabel>Password</FormLabel>
-                            <Input required placeholder='Password' type='password' value={Password} onChange={e => SetPassword(e.target.value)} />
-                            <FormLabel>Choose a Avtar</FormLabel>
-                            <Input className='inputAvtar' accept='image/*' id='avtar' onChange={AvtarHandler} type='file' required />
-                            <Button mt={''} color={'linkedin.300'} variant={'solid'} type='submit' >Sign UP</Button>
-                            <Text mt={'6'}> You have a accout <Link to={"/login"}>   <Button color={'linkedin.300'} variant={'link'} onClick={LoginHandler}>click</Button></Link>here</Text>
-                       </Stack>
-                        </FormControl>
-                    </form>
-                </Box>
-        </Container>
+    const [showPassword, setShowPassword] = useState(false);
+    return (
+        <>
+            <Flex
+                minH={'100vh'}
+                align={'center'}
+                justify={'center'}
+                bg={useColorModeValue('white', 'gray.800')}>
+                <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                    <Stack align={'center'}>
+                        <Heading fontSize={'4xl'} textAlign={'center'}>
+                            Sign up
+                        </Heading>
+                        <Text fontSize={'lg'} color={'gray.600'}>
+                            to enjoy all of our cool features ✌️
+                        </Text>
+                    </Stack>
+                    <Box
+                        rounded={'lg'}
+                        bg={useColorModeValue('white', 'gray.700')}
+                        boxShadow={'lg'}
+                        p={8}>
+                        <Stack w={'sm'} spacing={4}>
+                
+                            <form onSubmit={registerHandler}>
+
+
+                                <VStack>
+                                    <Avatar size={'lg'} src={AvatarPP} />
+                                </VStack>
+                                <FormControl id="Full_name" isRequired>
+                                    <FormLabel>Full Name</FormLabel>
+                                    <Input type="text"  value={Name} onChange={e => SetName(e.target.value)} />
+                                </FormControl>
+                                <FormControl isInvalid={isError} id="email" isRequired>
+                                    <FormLabel>Email address</FormLabel>
+                                    <Input  type='email' value={Email} onChange={e => SetEamil(e.target.value)} />
+                                {!isError ? (
+                                    <></>
+                                ) : (
+                                    <FormErrorMessage>Email is required.</FormErrorMessage>
+                                )}
+                                </FormControl>
+                                <FormControl id="password" isRequired>
+                                    <FormLabel>Password</FormLabel>
+                                    <InputGroup>
+                                        <Input type={showPassword ? 'text' : 'password'} value={Password} onChange={e => SetPassword(e.target.value)} />
+                                        <InputRightElement h={'full'}>
+                                            <Button
+                                                variant={'ghost'}
+                                                onClick={() =>
+                                                    setShowPassword((showPassword) => !showPassword)
+                                                }>
+                                                {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                                            </Button>
+                                        </InputRightElement>
+                                    </InputGroup>
+
+
+                                </FormControl>
+                                <FormControl isRequired>  <FormLabel>Choose a Avtar</FormLabel>
+                                    <Input  className='inputAvtar' accept='image/*' id='avtar' onChange={AvtarHandler} type='file' /></FormControl>
+                                <Stack mt={'3'} spacing={10} pt={2}>
+                                    <Button
+                                        loadingText="Submitting"
+                                        size="lg"
+                                        color={'linkedin.300'} variant={'solid'}
+                                        _hover={{
+                                            bg: 'gray.200',
+                                        }}
+                                        type={'submit'}>
+                                        Sign up
+                                    </Button>
+                                </Stack>
+                                <Stack pt={6}>
+                            
+                                    <Text >  Already a user? <Link to={"/login"}>   <Button color={'linkedin.300'} variant={'link'} >click</Button></Link>here</Text>
+
+                                </Stack>
+                            </form>
+                        </Stack>
+                    </Box>
+                </Stack>
+            </Flex>
+        </>
     )
 }
 export default Register
